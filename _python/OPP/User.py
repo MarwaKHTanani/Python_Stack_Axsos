062ea7e0112ba44825f213591f099721e0f9517e
MarwaKHTanani <tananimarwa@gmail.com>
2026-04-18 12:46:49 +0300

    edit user assignment

--- _python/OPP/User.py
+++ _python/OPP/User.py
@@ -19,10 +19,26 @@ class User:
 
 guido = User("Guido van Rossum")
 monty = User("Monty")
-monty.balance = 20
-guido.balance = 150
+alice=User('Alice')
+
+guido.deposit(100)
+guido.deposit(200)
+guido.deposit(300)
 guido.make_withdrawal(50)
 guido.display_user_balance()
-guido.transfer_money(monty, 50)
-guido.display_user_balance()
+
+monty.deposit(100)
+monty.deposit(200)
+monty.make_withdrawal(50)
+monty.make_withdrawal(20)
 monty.display_user_balance()
+
+alice.deposit(400)
+alice.make_withdrawal(50)
+alice.make_withdrawal(20)
+alice.make_withdrawal(10)
+alice.display_user_balance()
+
+guido.transfer_money(alice, 50)
+guido.display_user_balance()
+alice.display_user_balance()
