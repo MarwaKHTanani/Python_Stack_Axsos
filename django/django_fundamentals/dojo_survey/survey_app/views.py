48cd03e3acb5e545f0d99f12b15ae4af15f515f1
MarwaKHTanani <tananimarwa@gmail.com>
2026-05-30 18:50:07 +0300

    upload dojo survey assignment

---
+++ django/django_fundamentals/dojo_survey/survey_app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+
+def result(request):
+    context = {
+        "name": request.POST["name"],
+        "location": request.POST["location"],
+        "favorite_language": request.POST["favorite_language"],
+        "comment": request.POST["comment"],
+    }
+    return render(request, "result.html", context)
