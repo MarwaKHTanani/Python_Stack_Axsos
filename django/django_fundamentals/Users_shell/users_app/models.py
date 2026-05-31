9099f6982cd814de20b70a85e168aa70d0701e2d
MarwaKHTanani <tananimarwa@gmail.com>
2026-05-31 17:38:54 +0300

    upload multiple shell assignment

---
+++ django/django_fundamentals/Users_shell/users_app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    first_name=models.CharField(max_length=50)
+    last_name=models.CharField(max_length=50)
+    email=models.CharField(max_length=50)
+    age=models.IntegerField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)      
+    
+    def __str__(self):
+        return self.first_name 
+    
